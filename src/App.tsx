dac5142e9f037d9e45590f58f09a33c58635de21
SergikPinsk <fedukini@inbox.ru>
2023-04-20 12:56:11 +0300

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./Header/Header";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
-      <Header/>
-  );
+    return (
+        <div>
+            <Header/>
+            <Routes>
+                <Route path={'/'} element={<h1>Hello</h1>}></Route>
+                <Route path={'/Menu'} element={<h1>Menu</h1>}></Route>
+                <Route path={'/Select'} element={<h1>Select</h1>}></Route>
+            </Routes>
+        </div>
+
+
+    );
 }
 
 export default App;
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink, Route, Routes} from "react-router-dom";
+
 
 export const Header = ()=> {
     return (
             <div>
                 <NavLink to='/'>Home</NavLink>
+                <NavLink to='/Menu'>Menu</NavLink>
+                <NavLink to='/Select'>Select</NavLink>
             </div>
         )
 }
